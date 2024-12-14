a009439a4c8c2c4d1ba02819ce650465ea43ec68
ganeshdarga <118905448+ganeshdarga@users.noreply.github.com>
2024-12-14 23:08:11 +0530

    Update settings.py

--- phishingurldetector/phishingurldetector/settings.py
+++ phishingurldetector/phishingurldetector/settings.py
@@ -14,7 +14,9 @@ from pathlib import Path
 import dj_database_url
 
 from dotenv import load_dotenv
-load_dotenv() 
+env = environ.Env()
+
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
